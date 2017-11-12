840207cbcbef92171749ed7d6bb19e31c1c3eb3a
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2017-11-12 16:57:42 +0100

    Basic FamilyMember validation added
    Add: FamilyMember FirstName and LastName validation.
    
    Notes: It will expand, as more functionality will be implemented, there
    will be many validation scenarios for creating, editing FamilyMember etc.

---
+++ LocalClient/Core/Interfaces/IValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyBudget.LocalClient.Core.Interfaces
+{
+    public interface IValidator<T>
+    {
+        bool IsValid { get; set; }
+        bool Validate(T item);
+    }
+}
