977308f638967c862545f3f2212fc4b06dcd58ec
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2017-12-01 17:27:47 +0100

    WIP: Password en- and decoding prototype
    Started working on helpers for authentication, which will be responsible
    for encoding and decoding passwords.

---
+++ LocalClient/Components/Authentication/AuthenticationComponent/Helpers/PasswordDecoder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyBudget.LocalClient.Components.AuthenticationComponent.Helpers
+{
+    class PasswordDecoder
+    {
+
+        public static string Decode(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Decode(string password, string algorithm)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string DecodeInternal(string password, string algorithm)
+        {
+            string DecodedPassword = string.Empty;
+
+            switch (algorithm)
+            {
+                case "SHA256":
+                    DecodedPassword = DecodeSHA256(password);
+                    break;
+                case "SHA384":
+                    DecodedPassword = DecodeSHA384(password);
+                    break;
+                case "SHA512":
+                    DecodedPassword = DecodeSHA512(password);
+                    break;
+                case "AES":
+                    DecodedPassword = DecodeAES(password);
+                    break;
+            }
+
+            return DecodedPassword;
+        }
+
+        private static string DecodeSHA256(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string DecodeSHA384(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string DecodeSHA512(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string DecodeAES(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LocalClient/Components/Authentication/AuthenticationComponent/Helpers/PasswordEncoder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyBudget.LocalClient.Components.AuthenticationComponent.Helpers
+{
+    class PasswordEncoder
+    {
+
+        public static string Encode(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Encode(string password, string algorithm)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string EncodeInternal(string password, string algorithm)
+        {
+            string encodedPassword = string.Empty;
+
+            switch(algorithm)
+            {
+                case "SHA256":
+                    encodedPassword = EncodeSHA256(password);
+                    break;
+                case "SHA384":
+                    encodedPassword = EncodeSHA384(password);
+                    break;
+                case "SHA512":
+                    encodedPassword = EncodeSHA512(password);
+                    break;
+                case "AES":
+                    encodedPassword = EncodeAES(password);
+                    break;
+            }
+
+            return encodedPassword;
+        }
+
+        private static string EncodeSHA256(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string EncodeSHA384(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string EncodeSHA512(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string EncodeAES(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
