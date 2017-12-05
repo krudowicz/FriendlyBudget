049438f20addd2894f9fadc804c556d419554bed
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2017-12-05 17:45:12 +0100

    Started working on login page

---
+++ LocalClient/App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendlyBudget.LocalClient.Components.DAL.DTO;
+
+namespace FriendlyBudget.LocalClient.App.ViewModels
+{
+    class LoginViewModel
+    {
+        private User user;
+
+        public string LoginOrEmail { get; set; }
+        public string Password { get; set; }
+
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
