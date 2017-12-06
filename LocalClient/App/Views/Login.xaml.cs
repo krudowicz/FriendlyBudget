daf10c4b6b338f0175791f505db9a2a1ca762114
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2017-12-06 11:56:29 +0100

    WIP: LoginView

--- LocalClient/App/Views/Login.xaml.cs
+++ LocalClient/App/Views/Login.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FriendlyBudget.LocalClient.App.View
+namespace FriendlyBudget.LocalClient.App.Views
 {
     /// <summary>
     /// Interaction logic for Login.xaml
