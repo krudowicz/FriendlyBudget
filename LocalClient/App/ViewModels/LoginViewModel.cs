00d59ab397f847e90522c416b91fe93a30edb4cb
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2017-12-07 17:44:53 +0100

    Sync

--- LocalClient/App/ViewModels/LoginViewModel.cs
+++ LocalClient/App/ViewModels/LoginViewModel.cs
@@ -11,41 +11,14 @@ namespace FriendlyBudget.LocalClient.App.ViewModels
 {
     public class LoginViewModel : ViewModel
     {
-        private string _textBoxLabel;
-        private string _passwordBoxLabel;
+
 
         public string LoginOrEmail { get; set; }
         public string Password { get; set; }
-        public string LoginButtonCaption {get;set;}
-        public string TextBoxLabel
-        {
-            get { return _textBoxLabel; }
-            private set
-            {
-                _textBoxLabel = value;
-                NotifyPropertyChanged(TextBoxLabel);
-            }
-        }
-
-        public string PasswordBoxLabel
-        {
-            get { return _passwordBoxLabel; }
-            private set
-            {
-                _passwordBoxLabel = value;
-                NotifyPropertyChanged(PasswordBoxLabel);
-            }
-        }
-
+ 
         public RelayCommand LoginCommand { get; set; } 
 
         public LoginViewModel()
-        {
-            TextBoxLabel = "Login/Email";
-            PasswordBoxLabel = "Password";
-        }
-
-        public void LogIn()
         {
 
         }
