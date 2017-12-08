17c5ee66847caa2973293641b2a8d4280b120ea6
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2017-12-08 17:09:07 +0100

    Added viewmodels prototypes + sync

---
+++ LocalClient/App/ViewModels/FamilyMemberViewModel.cs
@@ -0,0 +1,21 @@
+﻿using FriendlyBudget.LocalClient.Components.DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyBudget.LocalClient.App.ViewModels
+{
+    class FamilyMemberViewModel
+    {
+
+        public FamilyMember FamilyMember { get; set; }
+        public ICollection<Expenditure> Expenditures { get; set; }
+
+        public FamilyMemberViewModel()
+        {
+
+        }
+    }
+}
---
+++ LocalClient/App/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyBudget.LocalClient.App.ViewModels
+{
+    class RegisterViewModel
+    {
+        public Dictionary<string, string> Labels { get; set; }
+
+        public RegisterViewModel()
+        {
+
+        }
+    }
+}
