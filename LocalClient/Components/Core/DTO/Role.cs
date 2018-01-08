ed7dc9e70b048d39ec01dda84ad597c60de3f39e
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2018-01-08 16:12:49 +0100

    [WIP] Rebuilding the project

--- LocalClient/Components/Core/DTO/Role.cs
+++ LocalClient/Components/Core/DTO/Role.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 
 namespace FriendlyBudget.LocalClient.Components.Core.DTO
 {
-    public class User : Person
+    public class Role : DataObject
     {
-        public string Login { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public Role Role { get; set; }
+        public string Name { get; set; }
+        public bool Read { get; set; }
+        public bool Write { get; set; }
     }
 }
---
+++ LocalClient/Components/Core/Enums/Enums/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendlyBudget.LocalClient.Components.Core.Enums
+{
+    public enum Role
+    {
+        Admin,
+        Normal,
+        Restricted,
+    }
+}
