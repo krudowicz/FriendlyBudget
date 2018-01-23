7b9f8b2eecdab5fd6141d6475dd7f90a72aca6ca
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2018-01-23 17:14:14 +0100

    Infrastructure init and tests

--- Web/Backend/Model/Domain Objects/Family.cs
+++ Web/Backend/Model/Domain Objects/Family.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FriendlyBudget.Web.Backend.Model.Value_Objects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,38 @@ namespace FriendlyBudget.Web.Backend.Model.Domain_Objects
 {
     class Family
     {
-        public ulong Id { get; private set; }
-        public IEnumerable<FamilyMember> FamilyMembers { get; private set; }
+        private ulong Id { get; set; }
+        private string Name { get; set; }
+        private IEnumerable<FamilyMember> FamilyMembers { get; set; }
+
+        public Family()
+        {
+            
+        }
+
+        public Money CalculateBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Money CalculateIncome()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Money CalculateExpenditure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Money CalculateAverageIncome()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Money CalculateAverageExpenditure()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
