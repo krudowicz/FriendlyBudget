aa337784cc85d36e432145131dbc44158584e4dd
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2018-01-23 22:42:18 +0100

    Database + entities init

---
+++ Web/Backend/Infrastructure/Database/MainContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FriendlyBudget.Web.Backend.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendlyBudget.Web.Backend.Infrastructure.Database
+{
+    class MainContext : DbContext()
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public DbSet<FamilyMember> FamilyMembers { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expenditure> Expenditures { get; set; }
+        public DbSet<Saving> Savings { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+    }
+}
---
+++ Web/Backend/Infrastructure/Entities/Family.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendlyBudget.Web.Backend.Infrastructure.Entities
+{
+    class Family
+    {
+        public ulong Id { get; set; }
+        public string Name { get; set; }
+        public List<FamilyMember> FamilyMembers { get; set; }
+        public string AddedAt { get; set; }
+        public string ModifiedAt { get; set; }
+    }
+}
