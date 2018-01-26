b930d1f003c9c2887135dfc00fc96d7853342bc5
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2018-01-26 15:07:38 +0100

    Role and UserDto added

---
+++ Web/Backend/Infrastructure/DTO/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendlyBudget.Web.Backend.Infrastructure.DTO
+{
+    class User
+    {
+        public ulong Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Web/Backend/Infrastructure/Entities/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendlyBudget.Web.Backend.Infrastructure.Entities
+{
+    class Role
+    {
+        public ulong Id { get; set; }
+        public string Name { get; set; }
+        public bool Read { get; set; }
+        public bool Write { get; set; }
+        public bool AddedAt { get; set; }
+        public bool ModifiedAt { get; set; }
+    }
+}
