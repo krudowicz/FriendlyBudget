1b6e8150c7ebf1c4b93167edd1d088c923085667
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2018-02-06 13:04:37 +0100

    [WIP]: Authentication - (I)AuthToken started

---
+++ Web/Backend/Model/Application Services/Authentication/Interfaces/IAuthToken.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendlyBudget.Web.Backend.Model.Application_Services.Authentication.Interfaces
+{
+    public interface IAuthToken
+    {
+        
+    }
+}
