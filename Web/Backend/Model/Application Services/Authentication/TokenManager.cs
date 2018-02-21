9d2aaa9226ecdeecb5a98c4cd8eb438220824e4f
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2018-02-21 11:42:06 +0100

    [WIP] TokenManager prototype

---
+++ Web/Backend/Model/Application Services/Authentication/TokenManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace FriendlyBudget.Web.Backend.Model.Application_Services.Authentication
+{
+    public class TokenManager
+    {
+        public Claim GenerateToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RevokeToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
