e8c7056a5122a20db8fcc1f3f7db0ba7f9c012bc
Krzysztof Rudowicz <krzysztof.rudowicz@gmail.com>
2018-02-21 12:42:15 +0100

    TM WIP

--- Web/Backend/Model/Application Services/Authentication/TokenManager.cs
+++ Web/Backend/Model/Application Services/Authentication/TokenManager.cs
@@ -1,23 +1,25 @@
-﻿using System;
+﻿using FriendlyBudget.Web.Backend.Model.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace FriendlyBudget.Web.Backend.Model.Application_Services.Authentication
 {
     public class TokenManager
     {
-        public Claim GenerateToken()
+        public JwtSecurityToken GenerateToken(IUser user)
         {
             throw new NotImplementedException();
         }
 
-        public bool ValidateToken()
+        public bool ValidateToken(JwtSecurityToken token)
         {
             throw new NotImplementedException();
         }
 
-        public void RevokeToken()
+        public void RevokeToken(JwtSecurityToken token)
         {
             throw new NotImplementedException();
         }
